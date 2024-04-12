856107f8ac1297e6f079a473745b010f052ef6d6
randepukperesoh <86397371+randepukperesoh@users.noreply.github.com>
2024-04-12 14:56:59 +0300

    Update db.js

--- back/db/db.js
+++ back/db/db.js
@@ -3,10 +3,9 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 5,
     host: "localhost",
-    user: "n91802ur_todos",
-    password: "Ma1234",
-    database: "n91802ur_todos"
+    user: "root",
+    password: "",
+    database: "Todos"
 });
 
 module.exports = pool;
-//https://free29.beget.com/phpMyAdmin/?pma_username=n91802ur_todos&pma_password=Ma1234&db=n91802ur_todos&server=1&uid=1712921961330
